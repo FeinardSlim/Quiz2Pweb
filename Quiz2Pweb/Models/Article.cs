150079f4f772bfbdac2861b1efe2937d5c89f8fa
FeinardSlim <58687783+FeinardSlim@users.noreply.github.com>
2020-11-12 23:33:02 +0700

    Add project files.

---
+++ Quiz2Pweb/Models/Article.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quiz2Pweb.Models
+{
+    public class Article
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public string Body { get; set; }
+    }
+
+    public class ArticleDBContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+    }
+}
---
+++ Quiz2Pweb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Quiz2Pweb.Startup))]
+namespace Quiz2Pweb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
